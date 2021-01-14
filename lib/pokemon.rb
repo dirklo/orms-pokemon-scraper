e7ce2cb90b5b4dc0f15d728f6556beb513358dc2
Rick <dirklo@gmail.com>
2021-01-14 09:48:40 +0100

    commit tree aa929726266b3bf82eeef28f761bb7dca0867edd

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(entry)
+        @id = entry[:id]
+        @name = entry[:name]
+        @type = entry[:type]
+        @db = entry[:db]
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?);
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT * FROM pokemon
+            WHERE id = ?;
+        SQL
+
+        pokemon = db.execute(sql, id)
+        object = {
+            :id=>pokemon[0][0],
+            :name=>pokemon[0][1],
+            :type=>pokemon[0][2],
+            :db=>db
+        }
+        Pokemon.new(object)
+    end
 end
